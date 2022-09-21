a156fc70bb439715518695fa7a05fb164ddc7dff
Nadine Kleine <nkgkleine@gmail.com>
2022-09-21 22:49:50 +0200

    subquery lab done

--- Lab_SQLSubqueries_3.02_Nadine.sql
+++ Lab_SQLSubqueries_3.02_Nadine.sql
@@ -72,10 +72,38 @@ WHERE country = 'CANADA';
 -- Task 6: Which are films starred by the most prolific actor? Most prolific actor is defined as the actor that has acted in the most number of films. 
     -- First you will have to find the most prolific actor and then use that actor_id to find the different films that he/she starred.
 
+SELECT film_id, title FROM film_actor
+LEFT JOIN film USING (film_id)
+WHERE actor_id = (SELECT actor_id 
+	FROM (SELECT actor_id, COUNT(film_id)
+			FROM film_actor
+            GROUP BY actor_id
+            ORDER BY COUNT(film_id) DESC
+            LIMIT 1) s1);
+
 
 -- Task 7: Films rented by most profitable customer. 
     -- You can use the customer table and payment table to find the most profitable customer 
     -- ie the customer that has made the largest sum of payments
 
-
+SELECT SUM(amount), title FROM film
+WHERE film_id IN(SELECT film_id 
+	FROM (SELECT film_id 
+		FROM inventory 
+		JOIN rental USING (inventory_id)
+		WHERE customer_id = (SELECT customer_id 
+			FROM (SELECT customer_id, SUM(amount) 
+				FROM payment 
+				GROUP BY customer_id
+				ORDER BY SUM(amount) DESC 
+                LIMIT 1) s1))s2);
+                
+                
 -- Task 8: Customers who spent more than the average payments.
+SELECT customer_id, SUM(amount) FROM payment
+GROUP BY customer_id
+HAVING SUM(amount) > (SELECT AVG() SUM(amount)
+	FROM (SELECT customer_id, SUM(amount) 
+		FROM payment
+		GROUP BY customer_id) s1);
+        
